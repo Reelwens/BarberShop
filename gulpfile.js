4a4544382935a2cbd2b25cee590f8e9198140156
Paul <paul.maire@hetic.net>
2016-12-13 10:44:08 +0100

    gulp watch change

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require( 'gulp' ),
   concat = require( 'gulp-concat' ),
   sourcemaps = require( 'gulp-sourcemaps' ),
   concat_css = require( 'gulp-concat-css' ),
-  minify = require( 'gulp-minify' );
+  minify = require( 'gulp-minify' ),
+  shell = require( 'gulp-shell' ),
+  cleanCSS = require( 'gulp-clean-css' );
 
 gulp.task( 'javascript', function () {
   return gulp.src( [ './src/js/clients/*.js', './src/js/items/*.js', './src/js/barber/*.js','./src/js/canvas/*.js', './src/js/*.js' ] )
@@ -23,3 +25,27 @@ gulp.task( 'css', function () {
     .pipe( concat_css( "css/bundle.css" ) )
     .pipe( gulp.dest( 'dist' ) );
 } );
+
+gulp.task( 'minify-css', function () {
+  return gulp.src( 'dist/css/bundle.css' )
+    .pipe( cleanCSS( {
+      debug: true
+    }, function ( details ) {
+      console.log( details.name + ': ' + details.stats.originalSize );
+      console.log( details.name + ': ' + details.stats.minifiedSize );
+    } ) )
+    .pipe( gulp.dest( 'dist/css/' ) );
+} );
+
+gulp.task( 'build', function () {
+  return gulp.src( 'package.json', {
+      read: false
+    } )
+    .pipe( shell( [
+      'npm run build'
+    ] ) )
+} )
+
+gulp.task( 'default', function () {
+  gulp.watch( [ './src/js/*.js', './src/js/**/*.js', './src/css/*.css', './index.html' ], [ 'build' ] );
+} )
