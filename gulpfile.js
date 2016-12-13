ae19ff1964060839d5a5b6ddeba3f46ab1e473eb
Paul <paul.maire@hetic.net>
2016-12-13 23:51:56 +0100

    seasons

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require( 'gulp' ),
   cleanCSS = require( 'gulp-clean-css' );
 
 gulp.task( 'javascript', function () {
-  return gulp.src( [ './src/js/config.js', './src/js/clients/*.js', './src/js/items/*.js', './src/js/barber/*.js', './src/js/canvas/*.js', './src/js/index.js' ] )
+  return gulp.src( [ './src/js/config.js', './src/js/clients/*.js', './src/js/items/*.js', './src/js/barber/*.js', './src/js/canvas/*.js', './src/js/index.js', './src/js/seasons/*.js' ] )
     .pipe( sourcemaps.init() )
     .pipe( concat( 'js/src.js' ) )
     .pipe( sourcemaps.write() )
