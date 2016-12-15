e5e905dd84b8fb9e21918c9a7819ea83c7682890
Simon - Reelwens <sim.lucas27@gmail.com>
2016-12-15 15:43:45 +0100

    Button mute, bruitage clic, modification fichiers mp3
    Boutton mute activante via la case en haut, à modifier

--- src/js/index.js
+++ src/js/index.js
@@ -20,3 +20,28 @@ document.querySelector('.container-left').addEventListener( 'click', function ()
         document.querySelector('.shop-container').classList.remove('clickListener');
     }, 150);
 });
+
+// Gestion bruitages
+var sound = document.querySelector('#soundEffect');
+
+document.querySelector('.container-left').addEventListener( 'click', function (){
+    var rand_1_3 = Math.round(((2)*Math.random()) + 1); // Generate random number between 1 and 3
+    sound.innerHTML = '<audio autoplay class="audioSound" src="src/sound/cut' + rand_1_3 + '.mp3"></audio>';
+    document.querySelector('.audioSound').volume=0.5;
+
+});
+
+// Gestion mute
+var mute = false,
+    muteButton = document.querySelector('.counter');
+
+muteButton.addEventListener( 'click', function (){
+    if (mute === false) {
+        document.querySelector('#sound').volume=0;
+        mute = true;
+    }
+    else {
+        document.querySelector('#sound').volume=0.15;
+        mute = false;
+    }
+});
