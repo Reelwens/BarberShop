aa779e58f23f11201c007e86a8e9ef2dc70af4a3
Simon - Reelwens <simonlucas.hetic@gmail.com>
2016-12-15 12:12:05 +0100

    Add sound, animation on click

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,17 @@ setInterval( function () {
     queue_init();
   }
 }, 1000 ); // TODO: Variabilize duration client apparition
+
+// Modification of the volume of the ambiance music
+document.querySelector('#sound').volume=0.15;
+
+// Animation for the shop when clic
+document.querySelector('.container-left').addEventListener( 'click', function () {
+    if (document.querySelector('.shop-container').classList.contains('clickListener')) 
+    return; // Protection for the clic while animation
+    
+    document.querySelector('.shop-container').classList.add('clickListener');
+    setTimeout(function(){
+        document.querySelector('.shop-container').classList.remove('clickListener');
+    }, 300);
+});
